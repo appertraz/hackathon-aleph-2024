d821157832661e6d8b490df905033994f278137a
Schmidt Cristian Hernán <schcriher@gmail.com>
2024-08-25 00:27:35 -0300

    add contribution page

--- packages/nextjs/app/contribution/page.tsx
+++ packages/nextjs/app/contribution/page.tsx
@@ -1,11 +1,45 @@
 "use client";
 
+import { useState } from "react";
 import type { NextPage } from "next";
+import { parseEther } from "viem";
+import { EtherInput } from "~~/components/scaffold-eth";
+import { useScaffoldWriteContract } from "~~/hooks/scaffold-eth";
 
 const Home: NextPage = () => {
+  const { writeContractAsync: writeControllerAsync } = useScaffoldWriteContract("Controller");
+  const [amount, setAmount] = useState("");
+
   return (
     <>
-      <div className="flex items-center flex-col flex-grow pt-10">PAGE</div>
+      <div className="flex items-center flex-col flex-grow pt-10">
+        <div className="px-5">
+          <h1 className="text-center">
+            <span className="block text-6xl mb-3 font-bold">Donar</span>
+            <span className="block text-2xl mb-3">Su donación es importante para el futuro de la sociedad</span>
+          </h1>
+        </div>
+
+        <div className="flex flex-col justify-center items-center flex-grow max-w-lg mt-8 mb-32 px-16">
+          <p className="block tracking-wide text-grey-darker font-bold mb-2">Ingrese la cantidad de Matic a donar</p>
+          <EtherInput value={amount} onChange={amount => setAmount(amount)} />
+          <button
+            className="btn btn-primary mt-4 w-40"
+            onClick={async () => {
+              try {
+                await writeControllerAsync({
+                  functionName: "deposit",
+                  value: parseEther(amount),
+                });
+              } catch (e) {
+                console.error("Error setting greeting:", e);
+              }
+            }}
+          >
+            Donar
+          </button>
+        </div>
+      </div>
     </>
   );
 };
--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -10,7 +10,8 @@ const Home: NextPage = () => {
           <h1 className="text-center">
             <span className="block text-2xl mb-3 mt-9">Bienvenido a la</span>
             <span className="block text-6xl mb-3 font-bold">Incubadora</span>
-            <span className="block text-2xl mb-3">De proyectos de vida</span>
+            <span className="block text-2xl mb-3">de proyectos de vida</span>
+            <span className="block text-1xl mb-3">enfocado en madres con hijos en la primera infancia</span>
           </h1>
         </div>
       </div>
